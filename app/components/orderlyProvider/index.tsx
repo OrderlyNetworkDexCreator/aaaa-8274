5008530ea7a942c46a9b86fc3faac948908db0a7
Wuzhong <wuzhong@orderly.network>
2026-03-27 09:00:04 +0800

    Modify light mode

--- app/components/orderlyProvider/index.tsx
+++ app/components/orderlyProvider/index.tsx
@@ -87,6 +87,98 @@ const CUSTOM_DARK_THEME_CSS_VARS: Partial<ThemeCssVars> = {
 	"--oui-spacing-xl": "33.75rem",
 };
 
+const CUSTOM_LIGHT_THEME_CSS_VARS: Partial<ThemeCssVars> = {
+	"--oui-font-family": "'HarmonyOS Sans', sans-serif",
+
+	/* colors */
+	"--oui-color-primary": "0 228 171",
+	"--oui-color-primary-light": "102 240 210",
+	"--oui-color-primary-darken": "0 180 135",
+	"--oui-color-primary-contrast": "255 255 255",
+
+	"--oui-color-link": "230 175 0",
+	"--oui-color-link-light": "252 213 53",
+
+	"--oui-color-secondary": "255 255 255",
+	"--oui-color-tertiary": "234 236 239",
+	"--oui-color-quaternary": "218 218 218",
+
+	"--oui-color-danger": "245 70 75",
+	"--oui-color-danger-light": "250 140 143",
+	"--oui-color-danger-darken": "220 40 45",
+	"--oui-color-danger-contrast": "255 255 255",
+	"--oui-color-success": "15 178 118",
+	"--oui-color-success-light": "90 210 165",
+	"--oui-color-success-darken": "10 140 90",
+	"--oui-color-success-contrast": "255 255 255",
+	"--oui-color-warning": "255 185 51",
+	"--oui-color-warning-light": "255 210 120",
+	"--oui-color-warning-darken": "220 150 20",
+	"--oui-color-warning-contrast": "255 255 255",
+
+	"--oui-color-fill": "245 245 245",
+	"--oui-color-fill-active": "238 238 238",
+
+	"--oui-color-base-1": "160 160 160",
+	"--oui-color-base-2": "210 210 210",
+	"--oui-color-base-3": "180 180 180",
+	"--oui-color-base-4": "200 202 205",
+	"--oui-color-base-5": "230 230 230",
+	"--oui-color-base-6": "245 245 245",
+	"--oui-color-base-7": "234 236 239",
+	"--oui-color-base-8": "255 255 255",
+	"--oui-color-base-9": "255 255 255",
+	"--oui-color-base-10": "245 245 245",
+
+	"--oui-color-base-foreground": "0 0 0",
+	"--oui-color-line": "0 0 0",
+
+	"--oui-color-base-static": "255 255 255",
+	"--oui-color-base-static-contrast": "0 0 0",
+
+	"--oui-color-trading-loss": "246 70 93",
+	"--oui-color-trading-loss-contrast": "255 255 255",
+	"--oui-color-trading-profit": "14 203 129",
+	"--oui-color-trading-profit-contrast": "255 255 255",
+
+	/* gradients */
+	"--oui-gradient-primary-start": "0 100 75",
+	"--oui-gradient-primary-end": "0 228 171",
+	"--oui-gradient-secondary-start": "0 140 105",
+	"--oui-gradient-secondary-end": "0 228 171",
+	"--oui-gradient-success-start": "5 90 60",
+	"--oui-gradient-success-end": "15 178 118",
+	"--oui-gradient-danger-start": "150 30 35",
+	"--oui-gradient-danger-end": "245 70 75",
+	"--oui-gradient-brand-start": "180 245 225",
+	"--oui-gradient-brand-end": "0 200 155",
+	"--oui-gradient-brand-stop-start": "6.62%",
+	"--oui-gradient-brand-stop-end": "86.5%",
+	"--oui-gradient-brand-angle": "17.44deg",
+	"--oui-gradient-warning-start": "180 110 10",
+	"--oui-gradient-warning-end": "255 185 51",
+	"--oui-gradient-neutral-start": "22 26 36",
+	"--oui-gradient-neutral-end": "34 40 52",
+
+	/* rounded */
+	"--oui-rounded-sm": "2px",
+	"--oui-rounded": "4px",
+	"--oui-rounded-md": "6px",
+	"--oui-rounded-lg": "8px",
+	"--oui-rounded-xl": "12px",
+	"--oui-rounded-2xl": "16px",
+	"--oui-rounded-full": "9999px",
+
+	/* spacing */
+	"--oui-spacing-xs": "20rem",
+	"--oui-spacing-sm": "22.5rem",
+	"--oui-spacing-md": "26.25rem",
+	"--oui-spacing-lg": "30rem",
+	"--oui-spacing-xl": "33.75rem",
+};
+
+
+
 const getNetworkId = (): NetworkId => {
 	if (typeof window === "undefined") return "mainnet";
 
@@ -269,7 +361,7 @@ const OrderlyProvider = (props: { children: ReactNode }) => {
 			id: "light",
 			displayName: "Light",
 			mode: "light",
-			cssVars: LIGHT_THEME_CSS_VARS,
+			cssVars: CUSTOM_LIGHT_THEME_CSS_VARS,
 		},
 	];
 
